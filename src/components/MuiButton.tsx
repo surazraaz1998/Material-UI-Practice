6403480ff391bb9aeeaec0df2dd85b2881450d49
Suraj Raj <srraaz1998@gmail.com>
2022-09-26 01:20:33 +0530

    initial commit

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,101 @@
+import {
+  Button,
+  ButtonGroup,
+  IconButton,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import { useState } from "react";
+
+const MuiButton = () => {
+    const [format, setFormat] = useState<string[]>([]);
+    console.log({format});
+    const handleFormatChange=(e:React.MouseEvent<HTMLElement>,updatedFormat:string[])=>{
+        setFormat(updatedFormat);
+    }
+
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" startIcon={<SendIcon />}>
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="secondary" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="medium"
+          color="secondary"
+          aria-label="aligment button group"
+        >
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <ToggleButtonGroup aria-label="text formatting" value={format} onChange={handleFormatChange}>
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="inderline">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">H1 Heading</Typography>
+      <Typography variant="h2">H2 Heading</Typography>
+      <Typography variant="h3">H3 Heading</Typography>
+      <Typography variant="h4">H4 Heading</Typography>
+      <Typography variant="h5">H5 Heading</Typography>
+      <Typography variant="h6">H6 Heading</Typography>
+      <Typography variant="subtitle1">subtitle1 Heading</Typography>
+      <Typography variant="subtitle2">subtitle2 Heading</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint quidem
+        eaque modi aperiam possimus, veritatis numquam nulla sed exercitationem
+        quam commodi accusantium ipsam blanditiis voluptatum a. Inventore
+        laborum nihil sit?
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+        repellat. Consequuntur, repudiandae. Tenetur eius labore, facere nisi
+        qui delectus vero numquam dicta, aliquid, modi consectetur non
+        laboriosam quasi recusandae amet.
+      </Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
